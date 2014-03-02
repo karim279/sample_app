49250090e34eb7eafd38c97b1b32262d5fa3eed3
Karim Sonbol <karim.sonbol279@gmail.com>
2014-03-02 11:48:00 +0200

    added contact page, used let function for rspec, changed database to postgres in development and test environment' '

--- spec/requests/staic_pages_spec.rb
+++ spec/requests/staic_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Staic pages" do
+	let(:base_title) {"Ruby on Rails Tutorial Sample App |"}
+
 	describe "Home page" do
 		it "should have the content 'Sample App'" do 
 			visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Staic pages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+			expect(page).to have_title("#{base_title} Home")
 		end
 	end
 
@@ -21,7 +23,7 @@ describe "Staic pages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+			expect(page).to have_title("#{base_title} Help")
 		end
 	end
 
@@ -33,7 +35,19 @@ describe "Staic pages" do
 
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+			expect(page).to have_title("#{base_title} About")
+		end
+	end
+
+	describe "Contact Page" do
+		it "should have the content 'Contact Us'" do
+			visit'/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+
+		it "shold have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} Contact")
 		end
 	end
 end
