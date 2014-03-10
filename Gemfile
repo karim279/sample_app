324dfab3cd0384127bd4100d3bb28d7748a2fb86
Karim Sonbol <karim.sonbol279@gmail.com>
2014-03-10 17:22:25 +0200

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ end
 group :test do
 	gem 'selenium-webdriver', '2.40.0'
 	gem 'capybara', '2.2.1'
+	gem 'factory_girl_rails', '4.4.1'
 end
 
 # Use SCSS for stylesheets
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,8 @@
 module UserHelper
+  	# Returns the Gravatar (http://gravatar.com/) for the given user.
+	def gravatar_for user
+		md5_hash = Digest::MD5::hexdigest(user.email.downcase)
+		link = "http://www.gravatar.com/avatar/#{md5_hash}"
+		image_tag link, alt: user.name, class: "gravatar"
+	end
 end
